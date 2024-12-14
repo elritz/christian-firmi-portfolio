61fcdfcd6fd9e30085f0f23fa90ee5baf07fcf17
Christian Firmi <christian@barfriends.com>
2024-12-13 22:05:06 -0500

    main: updated

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -9,78 +9,17 @@ export function BlogPosts() {
     return post;
   });
 
-  const blogsByType = allBlogs.reduce((acc, post) => {
-    const { type } = post.metadata;
-    if (!acc[type]) {
-      acc[type] = [];
-    }
-    acc[type].push(post);
-    return acc;
-  }, {});
-
-  return (
-    <div>
-      {Object.keys(blogsByType)
-        .map((type) => (
-          <div key={type}>
-            <h2 className='text-3xl capitalize font-bold mb-4'>{type}</h2>
-            <div className='grid grid-flow-row-dense gap-8 mx-auto grid-cols-2'>
-              {blogsByType[type]
-                .sort((a, b) => {
-                  if (new Date(a.metadata.date) > new Date(b.metadata.date)) {
-                    return -1;
-                  }
-                  return 1;
-                })
-                .map((post) => (
-                  <Link
-                    key={post.slug}
-                    className='flex flex-col space-y-1 mb-4'
-                    href={`/blog/${post.slug}`}
-                  >
-                    <Card>
-                      <div className='flex items-center justify-between gap-2'>
-                        <article className='relative w-full h-full p-4 md:p-8'>
-                          <div className='flex justify-between gap-2 items-center'>
-                            <span className='text-xs duration-1000 text-zinc-800 group-hover:text-black group-hover:border-zinc-800 drop-shadow-orange'>
-                              {post.metadata.date ? (
-                                <time
-                                  dateTime={new Date(
-                                    post.metadata.date
-                                  ).toISOString()}
-                                >
-                                  {Intl.DateTimeFormat(undefined, {
-                                    dateStyle: 'medium',
-                                  }).format(new Date(post.metadata.date))}
-                                </time>
-                              ) : (
-                                <span>SOON</span>
-                              )}
-                            </span>
-                            <div className='flex flex-col space-y-1'>
-                              <div>
-                                {post.metadata.current ? 'Current' : ''}
-                              </div>
-                            </div>
-                          </div>
-                          <h2 className='z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-800 group-hover:text-black font-display'>
-                            {post.metadata.title}
-                            <p className='text-base  duration-1000 text-zinc-800 group-hover:text-zinc-800'>
-                              {post.metadata.type}
-                            </p>
-                          </h2>
-                          <p className='z-20 mt-4 text-sm  duration-1000 text-zinc-800 group-hover:text-zinc-800'>
-                            {post.metadata.description}
-                          </p>
-                        </article>
-                      </div>
-                    </Card>
-                  </Link>
-                ))}
-            </div>
-          </div>
-        ))
-        .reverse()}
-    </div>
+  const blogsByType: { [key: string]: typeof allBlogs } = allBlogs.reduce(
+    (acc, post) => {
+      const { type } = post.metadata;
+      if (!acc[type]) {
+        acc[type] = [];
+      }
+      acc[type].push(post);
+      return acc;
+    },
+    {}
   );
+
+  return <div></div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -21,5 +21,53 @@ const navigation = [
   },
 ];
 export default function Home() {
-  return <div className='dark:prose-invert'></div>;
+  return (
+    <div className='dark:prose-invert'>
+      <div className='flex flex-col items-center justify-center overflow-x-hidden bg-gradient-to-tl from-white via-zinc-600/20 to-slate-200'>
+        <Particles
+          className='absolute inset-0 -z-10 animate-fade-in'
+          quantity={100}
+        />
+        <div className='h-screen flex  items-center justify-center w-screen'>
+          <div className='flex-1 py-11 flex-col'>
+            <nav className='flex justify-center animate-fade-in relative z-50'>
+              <ul className='flex align-center justify-center gap-4 z-50'>
+                {navigation.map((item, index) => (
+                  <Link
+                    key={index}
+                    target={item.target}
+                    href={item.href}
+                    className='text-md duration-200 text-zinc-500 hover:text-zinc-900 xs:text-md sm:text-md lg md:text-xl lg:text-xl flex flex-row justify-center items-center'
+                  >
+                    <p>{item.name}</p>
+                  </Link>
+                ))}
+              </ul>
+            </nav>
+            <div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
+            <h1 className='-z-10 mt-4 text-8xl  decoration-orange-500 text-transparent duration-1000 bg-black cursor-default text-edge-outline animate-title font-display  whitespace-nowrap bg-clip-text text-center'>
+              elritz
+            </h1>
+            <div className='hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
+            <div className='my-16 text-center animate-fade-in'>
+              <h2 className='text-md text-zinc-500 '>
+                Hi, my name is Christian, I&apos;m a software engineer. I work
+                with TypeScript and am currently working towards better
+                understanding Data structures and GOLANG <br />
+                and working at night on{' '}
+                <Link
+                  target='_blank'
+                  href='https://barfriends.com/'
+                  className='underline duration-500 hover:text-zinc-300'
+                >
+                  Barfriends
+                </Link>
+                .
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
